a545b8a1f1f3aee26d1c897c699d73292863e342
Keerthiga <ambikarajaboopathy7720@gmail.com>
2026-06-10 23:32:44 +0530

    Set DEBUG False

--- municipal_waste_monitoring_system/settings.py
+++ municipal_waste_monitoring_system/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!6px+!j!#7rg-=@imx_a)!jqtrwelftjr826(*xzx^d6n=y2&s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 

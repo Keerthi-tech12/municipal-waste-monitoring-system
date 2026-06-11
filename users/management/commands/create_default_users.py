4997849c9ceefed4da2fc007fcf49503df90ed65
Keerthiga <ambikarajaboopathy7720@gmail.com>
2026-06-11 18:30:42 +0530

    Fixed create_default_users command

--- users/management/commands/create_default_users.py
+++ users/management/commands/create_default_users.py
@@ -2,41 +2,43 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from users.models import UserProfile
 
+
 class Command(BaseCommand):
+
     help = 'Create default users'
 
     def handle(self, *args, **kwargs):
 
         users = [
-    ('admin', 'raja1505', True, True, 'Commissioner'),
-    ('commissioner', 'comm1505', False, False, 'Commissioner'),
-    ('keerthiga', 'keerthi1210', False, False, 'Commissioner'),
-    ('operator', 'oper1505', False, False, 'Data Entry Operator'),
-    ('zoneofficer', 'zone1505', False, False, 'Zone Officer'),
-]
+            ('admin', 'raja1505', True, True, 'Commissioner'),
+            ('commissioner', 'comm1505', False, False, 'Commissioner'),
+            ('keerthiga', 'keerthi1210', False, False, 'Commissioner'),
+            ('operator', 'oper1505', False, False, 'Data Entry Operator'),
+            ('zoneofficer', 'zone1505', False, False, 'Zone Officer'),
+        ]
 
-for username, password, is_staff, is_superuser, role in users:
+        for username, password, is_staff, is_superuser, role in users:
 
-    user, created = User.objects.get_or_create(
-        username=username
-    )
+            user, created = User.objects.get_or_create(
+                username=username
+            )
 
-    if created:
-        user.set_password(password)
+            if created:
+                user.set_password(password)
 
-    user.is_staff = is_staff
-    user.is_superuser = is_superuser
-    user.save()
+            user.is_staff = is_staff
+            user.is_superuser = is_superuser
+            user.save()
 
-    UserProfile.objects.get_or_create(
-        user=user,
-        defaults={
-            'role': role
-        }
-    )
+            UserProfile.objects.get_or_create(
+                user=user,
+                defaults={
+                    'role': role
+                }
+            )
 
-    self.stdout.write(
-        self.style.SUCCESS(
-            f'Created/Checked user: {username}'
-        )
-    )
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f'Created/Checked user: {username}'
+                )
+            )
